fe015b643bdfd7a322ccaf2c064a11f557f74f07
zakina <zakina@LAPTOP-H4212U29>
2024-08-14 11:51:26 +0200

    ajout class model Pays

---
+++ src/main/java/sio/paris2024/model/Pays.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sio.paris2024.model;
+
+/**
+ *
+ * @author zakina
+ */
+public class Pays {
+    private int id;
+    private String nom ;
+
+    public Pays() {
+    }
+
+    public Pays(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    
+    
+}
