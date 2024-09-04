fa833af361d6cb7bb35b1f7ec6014ec1856693b1
Minh-Tri <nguyen.minhtri@outlook.fr>
2024-09-04 10:05:10 +0200

    Ajout du prénom et dateNaiss à la requête

--- src/main/java/sio/paris2024/database/DaoAthlete.java
+++ src/main/java/sio/paris2024/database/DaoAthlete.java
@@ -5,6 +5,7 @@
 package sio.paris2024.database;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -26,7 +27,7 @@ public class DaoAthlete {
         
         ArrayList<Athlete> lesAthletes = new ArrayList<Athlete>();
         try{
-            requeteSql = cnx.prepareStatement("select a.id as a_id, a.nom as a_nom,  p.id as p_id, p.nom as p_nom " +
+            requeteSql = cnx.prepareStatement("select a.id as a_id, a.nom as a_nom, a.prenom as a_prenom, a.dateNaiss as a_dateNaiss, p.id as p_id, p.nom as p_nom " +
                          " from athlete a inner join pays p " +
                          " on a.pays_id = p.id ");
             //System.out.println("REQ="+ requeteSql);
@@ -37,7 +38,11 @@ public class DaoAthlete {
                 Athlete a = new Athlete();
                    a.setId(resultatRequete.getInt("a_id"));
                    a.setNom(resultatRequete.getString("a_nom"));
+                   a.setPrenom(resultatRequete.getString("a_prenom"));
                     
+                   Date date = resultatRequete.getDate("a_dateNaiss");
+                   a.setDateNaiss(date.toLocalDate());
+                   
                    Pays p = new Pays();
                    p.setId(resultatRequete.getInt("p_id"));
                    p.setNom(resultatRequete.getString("p_nom"));
@@ -59,7 +64,7 @@ public class DaoAthlete {
         
         Athlete a = new Athlete();
         try{
-            requeteSql = cnx.prepareStatement("select a.id as a_id, a.nom as a_nom,  p.id as p_id, p.nom as p_nom " +
+            requeteSql = cnx.prepareStatement("select a.id as a_id, a.nom as a_nom, a.prenom as a_prenom, a.datenaiss as a_datenaiss, p.id as p_id, p.nom as p_nom " +
                          " from athlete a inner join pays p " +
                          " on a.pays_id = p.id " + 
                          " where a.id = ? ");
@@ -97,8 +102,8 @@ public class DaoAthlete {
             // la paramètre RETURN_GENERATED_KEYS est ajouté à la requête afin de pouvoir récupérer l'id généré par la bdd (voir ci-dessous)
             // supprimer ce paramètre en cas de requête sans auto_increment.
             requeteSql=connection.prepareStatement("INSERT INTO athlete (nom, pays_id)\n" +
-                    "VALUES (?,?)", requeteSql.RETURN_GENERATED_KEYS );
-            requeteSql.setString(1, ath.getNom());      
+                    "VALUES (?,?,?,?)", requeteSql.RETURN_GENERATED_KEYS );
+            requeteSql.setString(1, ath.getNom());     
             requeteSql.setInt(2, ath.getPays().getId());
 
            /* Exécution de la requête */
