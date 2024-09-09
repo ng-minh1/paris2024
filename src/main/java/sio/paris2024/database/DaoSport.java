ba97aa39ae57091d7ee52729155ab21e2d6b503f
Minh-Tri <nguyen.minhtri@outlook.fr>
2024-09-09 11:16:53 +0200

    Création du DaoSport

---
+++ src/main/java/sio/paris2024/database/DaoSport.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sio.paris2024.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import sio.paris2024.model.Sport;
+
+/**
+ *
+ * @author SIO2
+ */
+public class DaoSport {
+    
+    Connection cnx;
+    static PreparedStatement requeteSql = null;
+    static ResultSet resultatRequete = null;
+    
+    public static ArrayList<Sport> getLesSports(Connection cnx){
+        
+        ArrayList<Sport> lesSports = new ArrayList<Sport>();
+        try{
+            requeteSql = cnx.prepareStatement("select s.id as s_id, s.libelle as s_libelle");
+            //System.out.println("REQ="+ requeteSql);
+            resultatRequete = requeteSql.executeQuery();
+            
+            while (resultatRequete.next()){
+                
+                Sport s = new Sport();
+                   s.setId(resultatRequete.getInt("s_id"));
+                   s.setLibelle(resultatRequete.getString("s_libelle"));
+                
+                lesSports.add(s);
+            }
+           
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("La requête de getLesSports e généré une erreur");
+        }
+        return lesSports;
+    }
+}
